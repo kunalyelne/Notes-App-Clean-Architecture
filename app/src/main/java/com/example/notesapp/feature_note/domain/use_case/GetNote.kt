5cd3c2b740e379559d51236b4060dbd4f301707c
Kunal <kunal17058@gmail.com>
2022-02-20 17:45:49 +0530

    Add minor changes

--- app/src/main/java/com/example/notesapp/feature_note/domain/use_case/GetNote.kt
+++ app/src/main/java/com/example/notesapp/feature_note/domain/use_case/GetNote.kt
@@ -6,7 +6,6 @@ import com.example.notesapp.feature_note.domain.repository.NoteRepository
 class GetNote(
     private val repository: NoteRepository
 ) {
-
     suspend operator fun invoke(id: Int): Note? {
         return repository.getNoteById(id)
     }
--- app/src/main/java/com/example/notesapp/feature_note/presentation/add_edit_note/AddEditNoteViewModel.kt
+++ app/src/main/java/com/example/notesapp/feature_note/presentation/add_edit_note/AddEditNoteViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.notesapp.feature_note.presentation.add_edit_note
 
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.example.notesapp.feature_note.domain.use_case.NoteUseCases
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -9,5 +11,11 @@ import javax.inject.Inject
 class AddEditNoteViewModel @Inject constructor(
     private val noteUseCases: NoteUseCases
 ) : ViewModel() {
+
+    private val _noteTitle = mutableStateOf(NoteTextFieldState())
+    val noteTitle: State<NoteTextFieldState> = _noteTitle
+
+    private val _noteContent = mutableStateOf(NoteTextFieldState())
+    val noteContent: State<NoteTextFieldState> = _noteContent
 
 }
